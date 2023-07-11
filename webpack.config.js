689fa9284add3f0fdbdfc0a30142e1a6d08a33c8
Andrea Licheri <126976233+andrealicheri@users.noreply.github.com>
2023-07-11 17:30:57 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './source.js',
+  output: {
+    filename: 'min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
+  
+  mode: 'production',
+};
